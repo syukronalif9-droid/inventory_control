104540dfb5838ac400c90201faed54b8889f9c40
sukronalif9-droid <syukronalif9@gmail.com>
2026-09-04 21:04:55 +0700

    Update logo to webp

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -28,7 +28,7 @@ const Login = ({ onLogin }) => {
   return (
     <div className="login-wrapper">
       <div className="login-card">
-        <img src="/ggf-logo.png" alt="Logo Great Giant Foods" className="login-logo" style={{ maxWidth: '300px', height: 'auto' }} />
+        <img src="/ggf-logo.webp" alt="Logo Great Giant Foods" className="login-logo" style={{ maxWidth: '300px', height: 'auto' }} />
         
         {errorMsg && <div className="error-message">{errorMsg}</div>}
 
